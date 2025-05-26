0dacf1c810cfd75d39e0e39b4bc2ae024b328284
Adebozz <ademolaadeigbe3@gmail.com>
2025-05-26 21:57:54 +0100

    `added width`

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { AuroraBackground } from "../components/ui/aurora-background";
 export default function Home() {
   return (
     <AuroraBackground>
-      <h3 className="text-2xl font-bold">Welcome Home</h3>
+      <h3 className="text-2xl font-bold w-10">Welcome Home</h3>
     </AuroraBackground>
   );
 }
