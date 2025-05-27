ade06a4dffda7c53fa2a92da2500886ff16c33a3
Adebozz <ademolaadeigbe3@gmail.com>
2025-05-27 15:54:26 +0100

    `Added semantic-ui-css and MenuExampleTabularOnLeft component to index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import 'semantic-ui-css/semantic.min.css';
 import { AuroraBackground } from "../components/ui/aurora-background";
+import MenuExampleTabularOnLeft from '../components/menubar';
 
 export default function Home() {
   return (
     <AuroraBackground>
       <h3 className="text-2xl font-bold w-10">Welcome Home</h3>
+      <MenuExampleTabularOnLeft />
     </AuroraBackground>
   );
 }
